e7efbe4762d38666e781cf6c71d06ac1f28a6519
Archit-Kayal <architkayal@gmail.com>
2024-03-24 23:40:46 +0530

    solved find_the_duplicate_no

--- main.cpp
+++ main.cpp
@@ -1,12 +1,49 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main(){
-    int x;
-    cin >> x;
-    for(int i=0;i<x;i++){
-        cout<<i<<endl;
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int n;
+        cin >> n;
+
+        int fl = 0;
+        vector<int> l(n);
+        for (int i = 0; i < n; ++i) {
+            cin >> l[i];
+        }
+
+        long long sum = 0;
+        for (int i = 0; i < n; ++i) {
+            sum += l[i];
+        }
+        if (sum == 0) {
+            cout << "YES" << endl;
+            continue;
+        }
+
+        int tmp = l[0];
+        for (int i = 1; i < n; ++i) {
+            if (l[i] >= tmp) {
+                l[i] -= tmp;
+                tmp = l[i];
+                l[i] = 0;
+            } else {
+                fl = 1;
+                break;
+            }
+        }
+
+        if (fl == 0) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
     }
+
     return 0;
 }
