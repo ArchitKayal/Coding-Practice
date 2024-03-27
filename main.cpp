48bc520c3554255158aaf6c75dbad1ac3e3e785b
Archit-Kayal <architkayal@gmail.com>
2024-03-27 19:50:35 +0530

    solved find_all_duplicates_in_an_array

--- main.cpp
+++ main.cpp
@@ -1,49 +1,22 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int main() {
-    int T;
-    cin >> T;
+int main(){
 
-    while (T--) {
-        int n;
-        cin >> n;
-
-        int fl = 0;
-        vector<int> l(n);
-        for (int i = 0; i < n; ++i) {
-            cin >> l[i];
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
         }
-
-        long long sum = 0;
-        for (int i = 0; i < n; ++i) {
-            sum += l[i];
+        for(int j=0;j<2*i+1;j++){
+            cout<<"*";
         }
-        if (sum == 0) {
-            cout << "YES" << endl;
-            continue;
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
         }
-
-        int tmp = l[0];
-        for (int i = 1; i < n; ++i) {
-            if (l[i] >= tmp) {
-                l[i] -= tmp;
-                tmp = l[i];
-                l[i] = 0;
-            } else {
-                fl = 1;
-                break;
-            }
-        }
-
-        if (fl == 0) {
-            cout << "YES" << endl;
-        } else {
-            cout << "NO" << endl;
-        }
+        cout<<endl;
     }
-
     return 0;
 }
